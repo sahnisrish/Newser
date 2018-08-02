0f35e1da736fc34614749114ce8980abde8ead0a
Paras Gupta <30497114+Parasgupta7@users.noreply.github.com>
2018-08-03 02:35:33 +0530

    Initial commit

---
+++ app/src/main/java/com/newser/Tab5.java
@@ -0,0 +1,55 @@
+package com.newser;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab5 extends Fragment {
+
+    private RecyclerView recyclerView;
+    private GridLayoutManager gridLayoutManager;
+//    private CustomAdapter adapter;
+//    private List<MyData> data_list;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_tab1, container, false);
+
+
+        return rootView;
+    }
+
+
+/*    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        data_list = new ArrayList<>();
+        //load_data_from_server(0);
+
+        gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        adapter = new CustomAdapter(getActivity(), data_list);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+                if (gridLayoutManager.findLastCompletelyVisibleItemPosition() == data_list.size() - 1) {
+                    //load_data_from_server(data_list.get(data_list.size() - 1).getId());
+                }
+
+            }
+        });
+    }
+*/
+}
+
